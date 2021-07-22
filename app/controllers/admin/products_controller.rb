86bc4738a017960391d9f32693bd97e634a815d9
mandatoryrecess <rossanneclamp@gmail.com>
2021-07-22 00:47:01 +0000

    http authentication password setup

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with :name => "jungle", :password => "book" 
-
+  
+  http_basic_authenticate_with name: ENV['AUTH_USER'], password: ENV['AUTH_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
