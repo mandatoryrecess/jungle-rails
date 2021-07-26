c5932519107950b0392de67eb878e4e0253028af
mandatoryrecess <rossanneclamp@gmail.com>
2021-07-26 20:44:08 +0000

    product_spec.rb added, with tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,46 @@
 require 'rails_helper'
 
+
 RSpec.describe Product, type: :model do
 
+    let(:category) { Category.create(name: "Home Decor") }
+
+    subject { Product.new(
+                  name: "Lamp",
+                  price_cents: 500,
+                  quantity: 4,
+                  category: category
+              )}
+
   describe 'Validations' do
-    # validation tests/examples here
+    it "is not valid without a valid name" do
+      subject.name = nil
+      puts subject.name
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with a valid price" do
+      subject.price_cents = nil
+      puts subject.price_cents
+      expect(subject).to_not be_valid
+    end
+
+    it "is valid with a valid quantity" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is valid with a valid category" do
+      subject.category = nil
+      puts category.id
+      puts category.name
+      expect(subject).to_not be_valid
+    end
+
+    it "is valid with valid attributes" do
+      puts subject.category.name
+      expect(subject).to be_valid
+    end
+
   end
-  
 end
